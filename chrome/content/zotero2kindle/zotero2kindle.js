74dcc1342d77f2c8a579d58fd25de49d9093342a
Robert Bamler <robert.bamler@gmail.com>
2013-02-24 16:35:59 +0100

    Clean up zotero2kindle for a client-only solution. Open xul tab on button click in zotero.

--- chrome/content/zotero2kindle/zotero2kindle.js
+++ chrome/content/zotero2kindle/zotero2kindle.js
@@ -3,37 +3,17 @@ Zotero.Zotero2kindle = {
 	
 	init: function () {
 		// Connect to (and create, if necessary) zotero2kindle.sqlite in the Zotero directory
-		this.DB = new Zotero.DBConnection('zotero2kindle');
+/*		this.DB = new Zotero.DBConnection('zotero2kindle');
 		
 		if (!this.DB.tableExists('changes')) {
 			this.DB.query("CREATE TABLE changes (num INT)");
 			this.DB.query("INSERT INTO changes VALUES (0)");
-		}
-		
-		// Register the callback in Zotero as an item observer
-		var notifierID = Zotero.Notifier.registerObserver(this.notifierCallback, ['item']);
-
-		var oldstop = Zotero.Sync.Runner.stop;
-		Zotero.Sync.Runner.stop = function () {
-			oldstop();
-			alert("snyc stopped");
-		}
-		var notifierID2 = Zotero.Notifier.registerObserver(this.notifierCallback, ['item']);
-		
-		// Unregister callback when the window closes (important to avoid a memory leak)
-		window.addEventListener('unload', function(e) {
-				Zotero.Notifier.unregisterObserver(notifierID);
-		}, false);
+		}*/
 	},
 	
 	sendSelectionToKindle: function() {
-		// Make sure data is synced (code adapted from zoteroPane.xul's <toolbarbutton id="zotero-tb-sync">)
-		Zotero.Sync.Server.canAutoResetClient = true;
-		Zotero.Sync.Server.manualSyncRequired = false;
-		Zotero.Sync.Runner.sync();	// seems to be asynchroneous
-
 		var selectedItems = ZoteroPane.getSelectedItems();
-		alert("selectedItems.length = " + selectedItems.length);
+//		alert("selectedItems.length = " + selectedItems.length);
 
 		// delete duplicates (e.g., if user selects both an attachment and its parent)
 		var entryIds = selectedItems.map(function(i) {
@@ -42,10 +22,10 @@ Zotero.Zotero2kindle = {
 		var uniqueEntryIds = entryIds.filter(function(elem, pos, self) {
 			return self.indexOf(elem) == pos;
 		})
-		alert("slectedEntryIds = " + JSON.stringify(uniqueEntryIds));
+//		alert("slectedEntryIds = " + JSON.stringify(uniqueEntryIds));
 
 		// generate list of all metadata
-		var postData = uniqueEntryIds.map(function(id) {
+		var docData = uniqueEntryIds.map(function(id) {
 			var i = Zotero.Items.get(id);
 			var attachmentKeys = i.getAttachments(false).map(function(id) {
 				var a_item = Zotero.Items.get(id);
@@ -66,7 +46,7 @@ Zotero.Zotero2kindle = {
 		});
 
 		// remove entries without attached PDF files
-		var noattach = postData.filter(function(elem) {
+		var noattach = docData.filter(function(elem) {
 			return elem.attachments.length == 0;
 		});
 		if (noattach.length) {
@@ -74,74 +54,21 @@ Zotero.Zotero2kindle = {
 				noattach.map(function(elem) { return elem.title; }).join("\n"));
 		}
 		
-		postData = postData.filter(function(elem) {
+		docData = docData.filter(function(elem) {
 			return elem.attachments.length != 0;
 		});
-		if (postData.length == 0) {
+		if (docData.length == 0) {
 			alert("No documents sent to your Kindle. Select an item with an attached PDF file and try again.");
 			return;
 		}
 
-		// TODO: save list to class variable. Make XHR call as soon as sync is finished.
-		alert(JSON.stringify(postData));
-	},
-	
-	// Callback implementing the notify() method to pass to the Notifier
-	notifierCallback: {
-		notify: function(event, type, ids, extraData) {
-//			alert("notify");
-/*			if (event == 'add' || event == 'modify' || event == 'delete') {
-				// Increment a counter every time an item is changed
-				Zotero.Zotero2kindle.DB.query("UPDATE changes SET num = num + 1");
-				
-				if (event != 'delete') {
-					// Retrieve the added/modified items as Item objects
-					var items = Zotero.Items.get(ids);
-				}
-				else {
-					var items = extraData;
-				}
-				
-				// Loop through array of items and grab titles
-				var titles = [];
-				for each(var item in items) {
-					// For deleted items, get title from passed data
-					if (event == 'delete') {
-						titles.push(item.old.title ? item.old.title : '[No title]');
-					}
-					else {
-						titles.push(item.getField('title'));
-					}
-				}
-				
-				if (!titles.length) {
-					return;
-				}
-				
-				// Get the localized string for the notification message and
-				// append the titles of the changed items
-				var stringName = 'notification.item' + (titles.length==1 ? '' : 's');
-				switch (event) {
-					case 'add':
-						stringName += "Added";
-						break;
-						
-					case 'modify':
-						stringName += "Modified";
-						break;
-						
-					case 'delete':
-						stringName += "Deleted";
-						break;
-				}
-				
-				var str = document.getElementById('zotero2kindle-strings').
-					getFormattedString(stringName, [titles.length]) + ":\n\n" +
-					titles.join("\n");
-			}
-			
-			alert(str);*/
-		}
+//		alert(JSON.stringify(docData));
+
+//		alert("opening tab");
+		var browserWindow = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+			.getService(Components.interfaces.nsIWindowMediator)
+			.getMostRecentWindow("navigator:browser");
+		var tab = browserWindow.gBrowser.loadOneTab("chrome://zotero2kindle/content/pdfcrop.xul", {inBackground:false});
 	}
 };
 
