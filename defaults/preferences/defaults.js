bae0649ea83370f067de7d23810dd8cbe89934ea
Robert Bamler <robert.bamler@gmail.com>
2013-05-22 07:40:03 +0200

    First draft of settings dialog.

---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,11 @@
+pref("extensions.zotero.dontprint.copyInGoogleDrive", false);
+pref("extensions.zotero.dontprint.copyToMe", false);
+pref("extensions.zotero.dontprint.k2pdfoptpath", "");
+pref("extensions.zotero.dontprint.outputdirectory", "");
+pref("extensions.zotero.dontprint.recipientEmailPrefix", "");
+pref("extensions.zotero.dontprint.recipientEmailSuffix", "@free.kindle.com");
+pref("extensions.zotero.dontprint.deviceClass", "kindle");
+pref("extensions.zotero.dontprint.screenWidth", 557);
+pref("extensions.zotero.dontprint.screenHeight", 721);
+pref("extensions.zotero.dontprint.sendEmail", true);
+pref("extensions.zotero.dontprint.placeInFolder", false);
