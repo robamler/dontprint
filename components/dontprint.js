robamler/dontprint

commit 183d3ba23105dbd374a9e5a4af1efff27dfa2db3

Set correct "title" meta-data of ouput PDF
This is important because Kobo e-readers apparently display the
title meta-data instead of the file name. We set the title field
by using already the finally intended file name for the temporarily
created file. It seems like k2pdfopt sets the title meta-data
to the output file name, at least if the input file did not already
hava a title field.

Fixes #15.

components/dontprint.js
@@ -1084,8 +1084,20 @@ function Dontprint() {
 	function convertDocument(job) {
 		updateJobState(job, "converting");
 		
+		// Create filename from author and title. We already use the final filename
+		// for the temporarily generated file because k2pdfopt generates the pdf
+		// title meta-data based on the output filename if the input file has none
+		// and these meta data are displayed by Kobo e-readers.
+		
+		// Don't allow the '.' char in the file name because files starting with
+		// a '.' are usually hidden on unix systems, which would be confusing.
+		job.finalFilename = job.title.replace(/[^a-zA-Z0-9 \-,]+/g, "_") + ".pdf";
+		if (job.articleCreators && job.articleCreators.length !== 0 && job.articleCreators[0].lastName) {
+			job.finalFilename = job.articleCreators[0].lastName.replace(/[^a-zA-Z0-9 \-,]+/g, "_") + ", " + job.finalFilename;
+		}
+		
 		let exec = getK2pdfopt();
-		let outFile = FileUtils.getFile("TmpD", ["dontprint-converted.pdf"]);
+		let outFile = FileUtils.getFile("TmpD", [job.finalFilename]);
 		outFile.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
 		job.convertedFilePath = outFile.path;
 		job.tmpFiles.push(outFile.path);
@@ -1159,21 +1171,10 @@ function Dontprint() {
 	}
 	
 	
-	function getFinalFilename(job) {
-		// Don't allow the '.' char in the file name because files starting with
-		// a '.' are usually hidden on unix systems, which would be confusing.
-		let ret = job.title.replace(/[^a-zA-Z0-9 \-_,]+/g, "_") + ".pdf";
-		if (job.articleCreators && job.articleCreators.length !== 0 && job.articleCreators[0].lastName) {
-			ret = job.articleCreators[0].lastName + ", " + ret;
-		}
-		return ret;
-	}
-	
-	
 	function sendEmail(job) {
 		updateJobState(job, "sending");
 		
-		job.emailedFilename = getFinalFilename(job);
+		job.emailedFilename = job.finalFilename;  // for compatibility of result page with older versions of Dontprint
 		job.recipientEmail = getRecipientEmail();
 		var url = buildURL(
 			"http://dontprint.net/cgi-bin/send-document.pl",
@@ -1263,7 +1264,7 @@ function Dontprint() {
 			throw 'The destination directory "' + destFile.path + '" does not exist. Maybe your device is not connected or it needs to be accessed under a different path.';
 		}
 		
-		destFile.append(getFinalFilename(job));
+		destFile.append(job.finalFilename);
 		destFile.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0775); // octal value --> don't remove leading zero!
 		
 		try {

commit a33fa5956b21dfd1cbe2620e483117dc890ef6e4

Fix severe issue with long file paths on Mac OS X
Fixes a bug introduced in commit 183d3ba. On Mac OS X, k2pdfopt
shortens long file paths for some reason. The effect of this was
that k2pdfopt generated a file with an unexpected filename if
the requested file path was too long. We work around this by
(1) using a shorter filename for the (temporary) output file of
k2pdfopt and (2) we run k2pdfopt from the directory of the
output file so that we can refer to it by relative path and
don't need to include the whole path in the command line
argument. Measure (1) does not affect the final name of the
file after the transfer/email step but it does affect the meta-
data that is included in the PDF document.

components/dontprint.js
@@ -721,6 +721,7 @@ function Dontprint() {
 			job.articlePages		= item.pages;
 			job.articleDate			= checkUndefined(item.date);
 			job.originalFilePath	= pdfFile.path;
+			job.originalFileName	= pdfFile.leafName;
 			itemDoneDeferred.resolve();
 			timeoutDeferred.resolve();
 		});
@@ -1092,13 +1093,18 @@ function Dontprint() {
 		
 		// Don't allow the '.' char in the file name because files starting with
 		// a '.' are usually hidden on unix systems, which would be confusing.
-		job.finalFilename = job.title.replace(/[^a-zA-Z0-9 \-,]+/g, "_") + ".pdf";
+		let authorAndTitle = job.title.replace(/[^a-zA-Z0-9 \-,]+/g, "_");
 		if (job.articleCreators && job.articleCreators.length !== 0 && job.articleCreators[0].lastName) {
-			job.finalFilename = job.articleCreators[0].lastName.replace(/[^a-zA-Z0-9 \-,]+/g, "_") + ", " + job.finalFilename;
+			authorAndTitle = job.articleCreators[0].lastName.replace(/[^a-zA-Z0-9 \-,]+/g, "_") + ", " + authorAndTitle;
 		}
+		job.finalFilename = authorAndTitle + ".pdf";
 		
+		// On OS X, k2pdfopt crops long file paths for some reason. To
+		// work around this issue, we start k2pdfopt with workdir set
+		// to the directory of the output file, and also use a rather
+		// short filename for the temporary file.
 		let exec = getK2pdfopt();
-		let outFile = FileUtils.getFile("TmpD", [job.finalFilename]);
+		let outFile = FileUtils.getFile("TmpD", [authorAndTitle.substr(0, 70) + ".pdf"]);
 		outFile.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
 		job.convertedFilePath = outFile.path;
 		job.tmpFiles.push(outFile.path);
@@ -1116,8 +1122,8 @@ function Dontprint() {
 			'-mr', '' + parseFloat(job.crop.m3)/25.4,
 			'-mb', '' + parseFloat(job.crop.m4)/25.4,
 			'-p', job.crop.pagerange ? job.crop.pagerange : (job.crop.coverpage ? '2-' : '1-'),
-			job.originalFilePath,
-			'-o', job.convertedFilePath
+			job.originalFileName!==undefined ? job.originalFileName : job.originalFilePath,
+			'-o', outFile.leafName
 		];
 		let globalArgs = prefs.getCharPref("k2pdfoptAdditionalParams").trim();
 		if (globalArgs) {
@@ -1133,6 +1139,7 @@ function Dontprint() {
 		
 		let p = subprocess.call({
 			command: exec,
+			workdir: outFile.parent.path,
 			arguments: args,
 			stdout: function(data) {
 				let lines = data.split(/[\n\r]+/);