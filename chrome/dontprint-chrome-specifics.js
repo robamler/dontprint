robamler/dontprint

commit 34de420cab405ad42064bae1ef122bfafe35e6f1

(Re-)Implement Dontprinting "zotero" typ jobs in portable version
Cleaning up some Firefox-specific code in common/dontprint.js
on the way. The cleaned-up code had already been moved to the
file dontprint-firefox-specifics.js.

common/dontprint.js
@@ -286,39 +286,6 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 	}
 	
 	
-// 	function dontprintLocalFile() { //TODO
-// 		let Dontprint = Components.classes['@robamler.github.com/dontprint;1']
-// 						.getService().wrappedJSObject;
-// 		let win = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-// 			.getService(Components.interfaces.nsIWindowMediator)
-// 			.getMostRecentWindow("navigator:browser");
-		
-// 		let nsIFilePicker = Components.interfaces.nsIFilePicker;
-// 		let fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-// 		fp.addToRecentDocs = true;
-// 		fp.init(win, "Pick a PDF document to send to your e-reader", nsIFilePicker.modeOpenMultiple);
-// 		fp.appendFilter("PDF documents", "*.pdf");
-// 		fp.appendFilters(nsIFilePicker.filterAll);
-		
-// 		if (fp.show() === nsIFilePicker.returnOK) {
-// 			let files = fp.files;
-// 			while (files.hasMoreElements())  {
-// 				let file = files.getNext().QueryInterface(Components.interfaces.nsILocalFile);
-// 				let m = file.leafName.match(/^(.*)\.pdf$/i);
-// 				let title = m ? m[1] : file.leafName;
-// 				Dontprint.runJob({
-// 					jobType:			"localfile",
-// 					title:				title,
-// 					journalLongname:	"",
-// 					journalShortname:	"",
-// 					originalFilePath:	file.path,
-// 					tmpFiles:			[]
-// 				});
-// 			}
-// 		}
-// 	}
-	
-	
 	function abortJob(jobId) {
 		let job = runningJobs[jobId];
 		try {
@@ -692,7 +659,6 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 		let job = runningJobs[jobId];
 		job.title = checkUndefined(item.title, "Untitled document");
 		job.articleCreators = item.creators;
-		job.authorsStr = getAuthorsString(item.creators);
 		job.journalLongname = checkUndefined(item.publicationTitle);
 		job.journalShortname = checkUndefined(item.journalAbbreviation);
 		job.doi = checkUndefined(item.DOI);
@@ -1195,34 +1161,6 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 			return creator.lastName;
 		}
 		return creator.firstName + " " + creator.lastName
-	}
-
-
-	function getAuthorsString(creators) {
-		switch (creators.length) {
-		case 0:
-			throw "error";
-		
-		case 1:
-			return formatName(creators[0]);
-		
-		case 2:
-			// two authors, separated by " and " without a comma
-			return formatName(creators[0]) + " and " + formatName(creators[1]);
-		
-		case 3: //FALLTHRU
-		case 4:
-			// comma separated list, with comma before the ", and "
-			var authorsStr = "";
-			for (var i=0; i<creators.length-1; i++) {
-				authorsStr += formatName(creators[i]) + ", ";
-			}
-			authorsStr += "and " + formatName(creators[creators.length-1]);
-			return authorsStr;
-		
-		default: // too many authors to list them all
-			return formatName(creators[0]) + " et al.";
-		}
 	}
 
 

firefox/chrome/content/dontprint/dontprint-firefox-specifics.js
@@ -1,7 +1,6 @@
 "use strict";
 
 Components.utils.import("resource://gre/modules/Timer.jsm");
-Components.utils.import("resource://EXTENSION/subprocess.jsm");
 
 (function() {
 	let Dontprint = PlatformTools.getMainComponent();
@@ -15,6 +14,8 @@ Components.utils.import("resource://EXTENSION/subprocess.jsm");
 	});
 
 	Components.utils.import("resource://EXTENSION/subprocess.jsm");
+	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+					.getService(Components.interfaces.nsIPromptService);
 
 
 	Dontprint.initOnPlatform = function() {
@@ -432,5 +433,133 @@ Components.utils.import("resource://EXTENSION/subprocess.jsm");
 				});
 			}
 		}
+	};
+
+
+	/**
+	 * Called when the user clicks the dontprint button in the Zotero pane.
+	 */
+	Dontprint.dontprintZoteroItems = function(items, forceCropWindow) {
+		// Create structure of all zotero "items" of which we want to dontprint
+		// at least one attachment. Items are indexed by their ID. To each item
+		// ID, we store a list of explicitly selected attachments, if any.
+		let itemmap = {};
+		for (let i=0; i<items.length; i++) {
+			let it = items[i];
+			let itemid = undefined;
+			let attachment = undefined;
+			if (it.isAttachment() && it.attachmentMIMEType === 'application/pdf') {
+				itemid = it.getSource();
+				attachment = it;
+			} else {
+				// Get ID of parent item (if, e.g., a note was selected) or of
+				// the selected item itself.
+				itemid = it.getSource() || it.id;
+			}
+			
+			if (itemid !== undefined) {
+				if (itemmap[itemid] === undefined) {
+					itemmap[itemid] = (attachment === undefined) ? [] : [attachment];
+				} else if (attachment !== undefined) {
+					itemmap[itemid].push(attachment);
+				}
+				// else do nothing (both the item and one of its attachments were selected)
+			}
+		}
+		
+		// Add first attachment to each item for which no attachment was explicitly selected
+		let noattach = [];
+		for (let id in itemmap) {
+			let val = itemmap[id];
+			if (val.length === 0) {
+				let attachs = Dontprint.Zotero.Items.get(id).getAttachments(false);
+				for (let j=0; j<attachs.length; j++) {
+					let attach = Dontprint.Zotero.Items.get(attachs[j]);
+					if (attach.attachmentMIMEType === 'application/pdf') {
+						val.push(attach);
+						break;
+					}
+				}
+				if (val.length === 0) {
+					noattach.push(id);
+					delete itemmap[id];
+				}
+			}
+		}
+		
+		// generate list of all meta data
+		let jobs = [];
+		for (let id in itemmap) {
+			let attachs = itemmap[id];
+			let i = Dontprint.Zotero.Items.get(id);
+			
+			// Note that item.getCreators() returns something different than
+			// what the "itemDone" handler of translators return. Apparently,
+			// Zotero manages a table of all creators of all stored items and
+			// therefore saves creators as references to the stored items.
+			// Here, we bring the job.aritcleCreators in the same form as for
+			// jobs of type "page" and make it JSONifyable (i.e., explicitly
+			// call the JavaScript getters for firstName and lastName).
+			let creators = [];
+			try {
+				creators = i.getCreators().map(function(c) {
+					return {
+						firstName: c.ref.firstName,
+						lastName:  c.ref.lastName
+					};
+				});
+			} catch (e) { } // fall back to empty array for creators
+			
+			for (let j=0; j<attachs.length; j++) {
+				// Note that if several attachments of the same item where selected,
+				// the corresponding jobs share the "creators" object. I.e., modifying
+				// job.articleCreators in one job will modify it in the others, too.
+				// But we never modify job.articleCreators, so this should not be an issue.
+				let file = attachs[j].getFile();
+				if (!file) {
+					prompts.alert(null, "Dontprint", "Error: Cannot find file for attachment to article \"" + i.getField('title') + "\".");
+					return;
+				}
+				jobs.push({
+					jobType:			"zotero",
+					title:				i.getField("title"),
+					journalLongname:	i.getField("publicationTitle"),
+					journalShortname:	i.getField("journalAbbreviation"),
+					pageurl:			i.getField("url"),
+					doi:				i.getField("DOI"),
+					articleDate:		i.getField("date"),
+					articleVolume:		i.getField("volume"),
+					articleIssue:		i.getField("issue"),
+					articlePages:		i.getField("pages"),
+					articleCreators:	creators,
+					forceCropWindow:	forceCropWindow,
+					origPdfFile: {
+						fullPath: file.path,
+						mozFile: file,
+						toURL: function() { return "file://" + file.path; }
+					}
+				});
+			}
+		}
+		
+		if (noattach.length !== 0) {
+			prompts.alert(
+				null,
+				"Dontprint",
+				"The following selected items cannot be sent to your e-reader because they do not have an attached PDF file:\n\n" +
+				noattach.map(function(id) {
+					return Dontprint.Zotero.Items.get(id).getField('title');
+				}).join("\n")
+			);
+		}
+		
+		if (jobs.length === 0) {
+			prompts.alert(null, "Dontprint", "Error: No documents sent to your e-reader. Select an item with an attached PDF file and try again.");
+			return;
+		}
+		
+		jobs.forEach(function(job) {
+			Dontprint.runJob(job);
+		});
 	};
 }());

commit 64d229d6dc25b967424cfecac58bbcc4212b6f6d

Fix "author" meta data of final PDF on Google Chrome
Commit 34de420ca introduced a bug that prevented the "author" meta
data field to be set correctly by k2pdfopt-nacl on Google Chrome (on
Firefox, we don't set any meta data anyway, at the moment). This
commit fixes that bug.

chrome/dontprint-chrome-specifics.js
@@ -85,6 +85,12 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
 				throw errorState;
 			}
 
+			try {
+				var authorsStr = getAuthorsString(job.articleCreators);
+			} catch (e) {
+				authorsStr = "";
+			}
+
 			// Add the final file to job.tmpFiles already now so that if
 			// conversion crashes or is canceled, the file will be removed
 			// during cleanup.
@@ -107,7 +113,7 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
 						]),
 						options: {
 							title: job.title,
-							author: job.authorsStr === undefined ? "" : job.authorsStr
+							author: authorsStr
 						}
 					});
 				});
@@ -209,4 +215,42 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
 	Dontprint.notifyJobDone = Zotero.Connector_Browser.dontprintJobDone;
 
 	Dontprint.postTranslate = postTranslate;
+
+
+	function getAuthorsString(creators) {
+		switch (creators.length) {
+		case 0:
+			throw "error";
+		
+		case 1:
+			return formatName(creators[0]);
+		
+		case 2:
+			// two authors, separated by " and " without a comma
+			return formatName(creators[0]) + " and " + formatName(creators[1]);
+		
+		case 3: //FALLTHRU
+		case 4:
+			// comma separated list, with comma before the ", and "
+			var authorsStr = "";
+			for (var i=0; i<creators.length-1; i++) {
+				authorsStr += formatName(creators[i]) + ", ";
+			}
+			authorsStr += "and " + formatName(creators[creators.length-1]);
+			return authorsStr;
+		
+		default: // too many authors to list them all
+			return formatName(creators[0]) + " et al.";
+		}
+	}
+
+	function formatName(creator) {
+		if (!creator.lastName) {
+			throw "error";
+		}
+		if (!creator.firstName) {
+			return creator.lastName;
+		}
+		return creator.firstName + " " + creator.lastName
+	}
 });

common/dontprint.js
@@ -1067,7 +1067,7 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 						if (job.result.success) {
 							resolve(job.result);
 						} else if (job.result.errno === 1) {
-							reject(new HtmlErrorMessage('The e-mail address you set in the preferences is invalid. Please go to the <a href="#" data-rpc="openSettings">Dontprint preferences</a> and make sure that the you set the correct e-mail address.'));
+							reject(new HtmlErrorMessage('The e-mail address you set in the preferences is invalid. Please go to the <a href="#" data-rpc="openSettings">Dontprint preferences</a> and make sure that you set the correct e-mail address.'));
 						} else if (job.result.errno === 3) {
 							PlatformTools.getPrefs({
 								verifiedEmails: []
@@ -1078,7 +1078,7 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 									})
 								});
 							}).then(function() {
-								reject(new HtmlErrorMessage('The e-mail address of your e-reader is not verified yet. Please go to the <a href="#" data-rpc="openSettings">Dontprint preferences</a>, make sure that the you set the correct e-mail address and then click the button labeled "Send verification code now".'));
+								reject(new HtmlErrorMessage('The e-mail address of your e-reader is not verified yet. Please go to the <a href="#" data-rpc="openSettings">Dontprint preferences</a>, make sure that you set the correct e-mail address and then click the button labeled "Send verification code now".'));
 							});
 						} else {
 							reject(job.result.message);
@@ -1142,17 +1142,6 @@ PlatformTools.registerMainComponent("Dontprint", function() {
 	function resultPageClosed(jobId) {
 		// Create a new fake job object, since the original one is already cleaned up
 		updateJobState({id: jobId}, "closed");
-	}
-
-
-	function formatName(creator) {
-		if (!creator.lastName) {
-			throw "error";
-		}
-		if (!creator.firstName) {
-			return creator.lastName;
-		}
-		return creator.firstName + " " + creator.lastName
 	}
 
 